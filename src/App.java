5ae787601d3afb027930ac1df4c80b7030cbeab6
jordanseasmlo@gmail.com <jordanseasmlo@gmail.com>
2026-09-10 20:41:28 -0600

    Commit de prueba

--- src/App.java
+++ src/App.java
@@ -1,7 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Hello, World! de Jordan");
 
-        System.out.println("Prueba 2");
+        System.out.println("Prueba ");
     }
 }
