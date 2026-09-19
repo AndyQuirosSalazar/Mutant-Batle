02e63a963e37d0e0195e7a8a2e5855a65235a96f
Andy Quiros <andyytj10@gmail.com>
2026-09-18 18:58:59 -0600

    Logica de poderes

--- src/Model/MutantPower.java
+++ src/Model/MutantPower.java
@@ -1,23 +1,56 @@
 package Model;
 
-public class MutantPower {
+import java.util.Random;
+
+public class MutantPower implements IConstants {
+
+    private static final Random RANDOM = new Random();
 
     public double powerId; // nombre de cada poder
     public int attackDamage; // aleatorio entre 1 y 3 con un máximo de 7
 
     public MutantPower() {
+        generateId();
+        this.attackDamage = assignDamage();
     }
 
     private int assignDamage() {
         // se asigna un daño aleatorio entre 1 y 3
-        return 0;
+        return MIN_DAMAGE + RANDOM.nextInt(3); // 1, 2 o 3
     }
 
     private void increaseDamage() {
-        // Se aumenta el daño en 1 cuando gana una batalla
+        // Se aumenta el daño en 1 cuando gana una batalla, sin pasar el máximo
+        if (attackDamage < MAX_DAMAGE) {
+            attackDamage++;
+        }
     }
 
     private void generateId() {
         // genera un Id aleatorio para cada poder
+        this.powerId = RANDOM.nextInt(1_000_000);
+    }
+
+    // Getters públicos para que otras clases (como Mutant) puedan usarlos
+    public int getAttackDamage() {
+        return attackDamage;
+    }
+
+    public double getPowerId() {
+        return powerId;
+    }
+
+    // Se necesita exponer increaseDamage() públicamente para que
+    // MutantController pueda llamarlo al terminar una batalla
+    public void onBattleWon() {
+        increaseDamage();
+    }
+
+    @Override
+    public String toString() {
+        return "MutantPower{" +
+                "powerId=" + powerId +
+                ", attackDamage=" + attackDamage +
+                '}';
     }
 }
