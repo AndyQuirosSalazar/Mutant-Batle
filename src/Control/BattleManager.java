f8d5ab6684b33377daf22ca46a2ca22c9440b08a
jordanseasmlo@gmail.com <jordanseasmlo@gmail.com>
2026-09-19 22:03:03 -0600

    Se termino Battlefield y cambios en battleManager

--- src/Control/BattleManager.java
+++ src/Control/BattleManager.java
@@ -1,5 +1,6 @@
 package Control;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class BattleManager {
@@ -7,9 +8,13 @@ public class BattleManager {
     public List<Thread> mutantThreads;
 
     public BattleManager() {
+        this.mutantThreads = new ArrayList<>();
     }
 
     public void startThreads() {
         // Inicia un hilo por cada mutante
+        for (Thread thread : mutantThreads) {
+            thread.start();
+        }
     }
 }
--- src/Game/Battlefield.java
+++ src/Game/Battlefield.java
@@ -1,15 +1,52 @@
 package Game;
 
+import Control.BattleManager;
+import Control.MutantController;
+import Model.Mutant;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class Battlefield {
 
     public Team team1; // objeto de la clase equipo
     public Team team2; // objeto de la clase equipo
     public Scoreboard scoreboard; // Guarda la cantidad de mutantes de cada equipo
+    public BattleManager battleManager; // Administra los hilos de los mutantes
+    public AtomicBoolean gameActive; // Compartida por todos los hilos, indica si la partida sigue en curso
 
-    public Battlefield() {
+    public Battlefield(Team team1, Team team2) {
+        this.team1 = team1;
+        this.team2 = team2;
+        startBattle();
     }
 
     private void startBattle() {
-        // para inicializar la batalla
+        // Arma el scoreboard con los equipos ya generados (mutantes, posiciones y energia
+        // se crean en el constructor de Team/Mutant), crea el estado compartido de la
+        // partida (bandera de juego activo y barrera de sincronizacion) y arranca un
+        // hilo MutantController por cada mutante, dejando el campo listo y en marcha.
+        scoreboard = new Scoreboard(team1, team2);
+        gameActive = new AtomicBoolean(true);
+
+        List<Mutant> allMutants = getAllMutants();
+        CyclicBarrier barrier = new CyclicBarrier(allMutants.size());
+
+        battleManager = new BattleManager();
+        for (Mutant mutant : allMutants) {
+            battleManager.mutantThreads.add(new Thread(new MutantController(mutant, this, barrier, gameActive)));
+        }
+
+        battleManager.startThreads();
+    }
+
+    // Lista combinada de mutantes de ambos equipos, para que Control la use en scanRadar/takeTurn
+    public List<Mutant> getAllMutants() {
+        return Stream.concat(Arrays.stream(team1.mutants), Arrays.stream(team2.mutants))
+                .collect(Collectors.toList());
     }
 }
