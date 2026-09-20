a024417b0ea74408259580559462c2d930dbe4df
jordanseasmlo@gmail.com <jordanseasmlo@gmail.com>
2026-09-19 20:31:57 -0600

    Se termino generador

--- src/Game/Generator.java
+++ src/Game/Generator.java
@@ -1,15 +1,21 @@
 package Game;
 
+import Model.Mutant;
+
 public class Generator {
 
     public Generator() {
     }
 
-    private void generateMutant() {
+    private Mutant generateMutant(double team) {
         // genera un mutante con todas sus características
+        // (id, defensa, poder y posición se generan dentro del constructor de Mutant)
+        return new Mutant(team);
     }
 
-    private void generateTeam() {
+    private Team generateTeam(double name, double mutantCount) {
         // genera un equipo con todas sus caracteristicas
+        // (el array de mutantes se genera dentro del constructor de Team)
+        return new Team(name, mutantCount);
     }
 }
